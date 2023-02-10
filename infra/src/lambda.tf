f38916d08ced373c8fae433cae354a1ef975a163
Ty Hopp <hopp.ty.c@gmail.com>
2023-02-10 19:17:17 +0800

    Fix lambda role, policy

--- infra/src/lambda.tf
+++ infra/src/lambda.tf
@@ -1,77 +1,71 @@
 resource "aws_lambda_function" "t-rss-reader-feeds-handler" {
   filename         = "feeds-handler.zip"
   function_name    = "t-rss-reader-feeds-handler"
-  role             = aws_iam_role.iam_for_lambda.arn
+  role             = aws_iam_role.t-rss-reader-handler-iam-role.arn
   handler          = "index.handler"
   source_code_hash = filebase64sha256("feeds-handler.zip")
   runtime          = "nodejs18.x"
 }
 
-resource "aws_lambda_event_source_mapping" "t-rss-reader-db-handler-event-source-mapping" {
-  event_source_arn  = aws_dynamodb_table.t-rss-reader-feeds-table.stream_arn
-  function_name     = aws_lambda_function.t-rss-reader-feeds-handler.arn
-  starting_position = "LATEST"
-}
-
-resource "aws_iam_role" "t-rss-reader-handler-iam" {
+resource "aws_iam_role" "t-rss-reader-handler-iam-role" {
   name = "t-rss-reader-handler-iam"
-
-  assume_role_policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Action": "sts:AssumeRole",
-          "Principal": {
-            "Service": "lambda.amazonaws.com"
-          },
-          "Effect": "Allow",
-          "Sid": ""
+  assume_role_policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [
+      {
+        "Action" = "sts:AssumeRole"
+        "Effect" = "Allow"
+        "Sid"    = ""
+        "Principal" = {
+          "Service" = "lambda.amazonaws.com"
         }
-      ]
-    }
-  EOF
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy" "t-rss-reader-db-handler-iam" {
-  name   = "t-rss-reader-db-handler-iam"
-  role   = aws_iam_role.t-rss-reader-handler-iam.id
-  policy = <<EOF
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-            "Sid": "AllowLambdaFunctionToCreateLogs",
-            "Action": [ 
-                "logs:*" 
-            ],
-            "Effect": "Allow",
-            "Resource": [ 
-                "arn:aws:logs:*:*:*" 
-            ]
-        },
-        {
-            "Sid": "AllowLambdaFunctionInvocation",
-            "Effect": "Allow",
-            "Action": [
-                "lambda:InvokeFunction"
-            ],
-            "Resource": [
-                "${aws_dynamodb_table.t-rss-reader-feeds-table.arn}/stream/*"
-            ]
-        },
-        {
-            "Sid": "APIAccessForDynamoDBStreams",
-            "Effect": "Allow",
-            "Action": [
-                "dynamodb:GetRecords",
-                "dynamodb:GetShardIterator",
-                "dynamodb:DescribeStream",
-                "dynamodb:ListStreams"
-            ],
-            "Resource": "${aws_dynamodb_table.t-rss-reader-feeds-table.arn}/stream/*"
-        }
-      ]
-    }
-  EOF
+resource "aws_iam_role_policy" "t-rss-reader-handler-iam-policy-logs" {
+  name = "t-rss-reader-handler-iam-policy-logs"
+  role = aws_iam_role.t-rss-reader-handler-iam-role.id
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [
+      {
+        "Effect"   = "Allow",
+        "Action"   = "logs:CreateLogGroup",
+        "Resource" = "arn:aws:logs:ap-southeast-1:*:*"
+      },
+      {
+        "Effect" = "Allow",
+        "Action" = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" = [
+          "arn:aws:logs:ap-southeast-1:${aws_dynamodb_table.t-rss-reader-feeds-table.arn}:log-group:/aws/lambda/t-rss-reader-feeds-handler:*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "t-rss-reader-handler-iam-policy-dynamodb" {
+  name = "t-rss-reader-handler-iam-policy-dynamodb"
+  role = aws_iam_role.t-rss-reader-handler-iam-role.id
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Statement" = [
+      {
+        "Effect" = "Allow",
+        "Action" = [
+          "dynamodb:DeleteItem",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:UpdateItem"
+        ],
+        "Resource" = "arn:aws:dynamodb:ap-southeast-1:${aws_dynamodb_table.t-rss-reader-feeds-table.arn}:table/*"
+      }
+    ]
+  })
 }
