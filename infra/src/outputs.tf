d193f572a4edac853bf2112dbb66d03a2a9d3f5d
tyhopp <hopp.ty.c@gmail.com>
2023-02-13 17:09:49 +0800

    Add login endpoint tf output

--- infra/src/outputs.tf
+++ infra/src/outputs.tf
@@ -1,4 +1,9 @@
-output "t-rss-reader-invoke-url" {
-  description = "Invoke url"
-  value       = "https://${aws_apigatewayv2_api.t-rss-reader-feeds-handler-api.id}.execute-api.${var.aws-region}.amazonaws.com/default"
+output "t-rss-reader-login-handler-invoke-url" {
+  description = "Login handler invoke url"
+  value       = "https://${aws_apigatewayv2_api.t-rss-reader-login-handler-api.id}.execute-api.${var.aws-region}.amazonaws.com/default/login"
+}
+
+output "t-rss-reader-feeds-handler-invoke-url" {
+  description = "Feeds handler invoke"
+  value       = "https://${aws_apigatewayv2_api.t-rss-reader-feeds-handler-api.id}.execute-api.${var.aws-region}.amazonaws.com/default/feeds"
 }
