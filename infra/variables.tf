34de9fb5b61c24f734b7d95105068a16434bca01
Ty Hopp <hopp.ty.c@gmail.com>
2023-02-22 19:15:30 +0800

    Switch to local Terraform execution because cloud is slow

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,27 @@
 variable "t-rss-reader-password" {
   description = "The password to use for authenticating requests"
-  type = string
-  sensitive = true
-  nullable = false
+  type        = string
+  sensitive   = true
+  nullable    = false
 }
 
 variable "t-rss-reader-origin" {
   description = "The origin to configure CORS with"
-  type = string
-  default = "http://localhost:8000"
+  type        = string
+  default     = "http://localhost:8000"
+}
+
+variable "aws-access-key" {
+  description = "Public key for accessing AWS resources"
+  type        = string
+  nullable    = false
+}
+
+variable "aws-secret-access-key" {
+  description = "Secret key for accessing AWS resources"
+  type        = string
+  sensitive   = true
+  nullable    = false
 }
 
 variable "aws-region" {
