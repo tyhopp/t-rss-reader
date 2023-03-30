e912a10886ade60004896a21a40c85ed26ec1963
tyhopp <hopp.ty.c@gmail.com>
2023-03-30 17:51:55 +0800

    Remove dynamo db provisioning

--- infra/feeds-dynamo-db.tf
+++ infra/feeds-dynamo-db.tf
@@ -1,9 +1,7 @@
 resource "aws_dynamodb_table" "t-rss-reader-feeds-table" {
-  name           = "t-rss-reader-feeds-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "url"
+  name         = "t-rss-reader-feeds-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "url"
 
   attribute {
     name = "url"
--- infra/last-access-dynamo-db.tf
+++ infra/last-access-dynamo-db.tf
@@ -1,9 +1,7 @@
 resource "aws_dynamodb_table" "t-rss-reader-last-access-table" {
-  name           = "t-rss-reader-last-access-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "id"
+  name         = "t-rss-reader-last-access-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attribute {
     name = "id"
