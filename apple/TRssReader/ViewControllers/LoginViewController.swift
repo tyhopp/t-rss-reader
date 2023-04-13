38fd7ca077b11415dc19addfba6156fc04cd7551
tyhopp <hopp.ty.c@gmail.com>
2023-04-13 14:59:16 +0800

    Introduce view controllers

---
+++ apple/TRssReader/ViewControllers/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  TRssReader
+//
+//  Created by Ty Hopp on 13/4/23.
+//
+
+import SwiftUI
+
+// TODO: Implement
+struct LoginViewController: View {
+    var body: some View {
+        Text("Login view controller")
+    }
+}
+
+struct LoginViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginViewController()
+    }
+}
