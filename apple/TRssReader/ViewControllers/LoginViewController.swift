9cacae0e088f57e5661be2cfec95078c84d1022f
tyhopp <hopp.ty.c@gmail.com>
2023-04-13 16:14:27 +0800

    Login view controller foundation

--- apple/TRssReader/ViewControllers/LoginViewController.swift
+++ apple/TRssReader/ViewControllers/LoginViewController.swift
@@ -9,8 +9,59 @@ import SwiftUI
 
 // TODO: Implement
 struct LoginViewController: View {
+    @State private var password: String = ""
+    
+    private var loginService: LoginService = LoginService()
+    private var keychainManager: KeychainManager = KeychainManager()
+    
+    func submit() async {
+        do {
+            let (loginData, loginResponse) = try await loginService.login(password: password)
+            
+            guard let token = try? JSONDecoder().decode(Token.self, from: loginData) else {
+                // TODO: Display error state
+                print("Failed to login")
+                return
+            }
+
+            guard loginResponse.statusCode() == 200 && !token.accessToken.isEmpty else {
+                // TODO: Display error state
+                print("Failed to login")
+                return
+            }
+            
+            keychainManager.setToken(token: loginData)
+        } catch {
+            // TODO: Display error state
+            print(error)
+        }
+    }
+    
     var body: some View {
-        Text("Login view controller")
+        VStack {
+            Text("Enter your password")
+            SecureField("Password", text: $password) {
+                Task {
+                    await submit()
+                }
+            }
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            // TODO: Display loading state
+            Button("Log In", action: {
+                Task {
+                    await submit()
+                }
+            })
+            Spacer()
+        }
+        .frame(
+            idealWidth: 300.0,
+            maxWidth: 300.0,
+            alignment: .center
+        )
+        .padding()
+        .navigationTitle("t-rss-reader")
     }
 }
 
