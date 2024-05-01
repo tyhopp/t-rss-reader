6937b648f9392c94fdeff98d865156927a20e743
Ty Hopp <hopp.ty.c@gmail.com>
2024-05-01 18:46:34 +0800

    Add favicons, manifest to web client

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -22,9 +22,12 @@
   />
   <link rel="apple-touch-icon-precomposed" sizes="180x180" href="favicon-180.jpg" />
   <link rel="apple-touch-icon" sizes="180x180" href="favicon-180.jpg" />
-  <link rel="icon" type="image/jpg" sizes="48x48" href="favicon-48.jpg" />
-  <link rel="icon" type="image/jpg" sizes="32x32" href="favicon-32.jpg" />
-  <link rel="icon" type="image/jpg" sizes="16x16" href="favicon-16.jpg" />
+  <link rel="icon" type="image/jpeg" sizes="512x512" href="favicon-512.jpg" />
+  <link rel="icon" type="image/jpeg" sizes="48x48" href="favicon-48.jpg" />
+  <link rel="icon" type="image/jpeg" sizes="32x32" href="favicon-32.jpg" />
+  <link rel="icon" type="image/jpeg" sizes="16x16" href="favicon-16.jpg" />
+  <link rel="icon" href="favicon.ico" />
+  <link rel="manifest" href="manifest.json">
   <meta property="og:type" content="website" />
   <meta property="og:image" content="open-graph-card.jpg" />
   <meta property="og:image:width" content="1200" />
